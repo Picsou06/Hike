7d7a4eff24bc069fbf51e6c4441e9023d73056c8
Evan D <evan@ecaste.com>
2025-08-11 11:51:12 +0200

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Picsou 06 on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HikeTests/HikeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HikeTests.swift
+//  HikeTests
+//
+//  Created by Picsou 06 on 11/08/2025.
+//
+
+import Testing
+@testable import Hike
+
+struct HikeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
